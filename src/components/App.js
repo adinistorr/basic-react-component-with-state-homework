d57a868c32ae0e922fd9bcc26653c0a335a3e9f9
Adi Nistor <54371888+adinistorr@users.noreply.github.com>
2020-09-16 17:52:54 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter';
+import ChangeColor from './ChangeColor';
+
+function App() {
+    return(
+        <div>
+            <h1>Hello from App Component!</h1>
+            <Counter />
+            <ChangeColor />
+        </div>
+    )
+}
+
+export default App;
